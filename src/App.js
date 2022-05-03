c6f7487fe32d9544002b35212fe7f46ed3c64b1f
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-05-03 12:59:16 +0300

    manasseh's initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginPage from './login/LoginPage';
+import NavBar from './navbar/NavBar';
+import Profile from './profile/Profile';
 
 function App() {
   return (
     <div >
-      
+      <NavBar/>
+      <Profile/>
+      <LoginPage/>
     </div>
   );
 }
---
+++ src/navbar/NavBar.js
@@ -0,0 +1,20 @@
+function NavBar() {
+    return(
+        <header className="main-head">
+            <nav>
+                <div>
+                    <h1 id="logo">LOGO</h1>
+                </div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">LogIn/Sign Up</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
