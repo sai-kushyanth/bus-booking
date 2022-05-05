2af9998564bc8325b716c518f80d35714080cb06
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-05-05 10:29:48 +0300

    added dashboard components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Home from './home/Home';
+import Dashboard from './dashboard/dashboard';
 import './App.css';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Home/>
+      
      
     </div>
   );
