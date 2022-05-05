ec4b7c58bb99d11c65e4c45951cf02824b77233f
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-05-05 11:39:57 +0300

    added browser router

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import NavBar from './navbar/NavBar';
 import Profile from './profile/Profile';
 
 import Home from './home/Home';
+import  Dashboard from './dash'
 import './App.css';
 import Main from './signup/signup'
 import Book from './signup/register';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 
 
 function App() {
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element ={<Home/>}/>
+        <Route path="Dashboard" element={<Dashboard/>}/>
 
+      </Routes>
+    
+    </BrowserRouter>
 
   );
 }
